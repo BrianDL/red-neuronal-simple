d3824195f8fe32590b6f0a94c212fc3a395b1d6b
Brian Leiva <bridleiva@gmail.com>
2024-11-02 23:31:45 -0600

    adding nix files

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    zig
+    git
+    texlive.combined.scheme-full
+    gnumake
+  ];
+
+  shellHook = ''
+    echo "Welcome to the red-neuronal-simple development environment!"
+    echo "Zig version: $(zig version)"
+    echo "Git version: $(git --version)"
+    echo "LaTeX version: $(latex --version | head -n 1)"
+    echo "GNU Make version: $(make --version | head -n 1)"
+  '';
+}
