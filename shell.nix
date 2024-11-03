490a241a3039d0da947e8dd539211a08d61dc036
Brian Leiva <bridleiva@gmail.com>
2024-11-02 23:57:48 -0600

    removing extra packages

--- shell.nix
+++ shell.nix
@@ -3,16 +3,16 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     zig
-    git
-    texlive.combined.scheme-full
-    gnumake
+    # git
+    # texlive.combined.scheme-full
+    # gnumake
   ];
 
   shellHook = ''
     echo "Welcome to the red-neuronal-simple development environment!"
     echo "Zig version: $(zig version)"
-    echo "Git version: $(git --version)"
-    echo "LaTeX version: $(latex --version | head -n 1)"
-    echo "GNU Make version: $(make --version | head -n 1)"
+    # echo "Git version: $(git --version)"
+    # echo "LaTeX version: $(latex --version | head -n 1)"
+    # echo "GNU Make version: $(make --version | head -n 1)"
   '';
 }
